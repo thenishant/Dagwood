a32c5015d31d42ae4e8d3f71e5ab35128a5d3c4b
Nishant <nishant.s.aux@go-jek>
2019-06-29 11:42:45 +0530

    Code cleanUp

--- Appium_Test/docker-config.sh
+++
@@ -1,6 +0,0 @@
-#Get device names
-devices=$(adb devices | grep -oP "\K([^ ]+)(?=\sdevice(\W|$))")
-echo "Devices found: ${#devices[@]}"
-
-APPIUM_LOG="/var/log/appium.log"
-CMD="xvfb-run appium --log $APPIUM_LOG"
