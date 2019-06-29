b279d949699a712c65df2c7c24b523b84ee1b9fd
Nishant <nishant.s.aux@go-jek>
2019-06-29 11:44:09 +0530

    Update vnc port

--- Appium_Test/src/main/java/utils/DriverFactory.java
+++ Appium_Test/src/main/java/utils/DriverFactory.java
@@ -52,36 +52,3 @@ public class DriverFactory {
         return driver;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-#!/bin/sh
-
-        password="android"
-        password_view_only="docker"
-
-        /usr/bin/expect <<EOF
-        spawn vncpasswd
-        expect "Password:"
-        send "$password\r"
-        expect "Verify:"
-        send "$password\r"
-        expect "Would you like to enter a view-only password (y/n)?"
-        send "y\r"
-        expect "Password:"
-        send "$password_view_only\r"
-        expect "Verify:"
-        send "$password_view_only\r"
-        expect eof
-        exit
-        EOF
